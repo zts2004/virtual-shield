f95e1774c3996cfd504ce502464ed31675e88f28
Your Name <3360253688@qq.com>
2025-03-25 22:16:43 +0800

    添加：集成阿里云OSS实现文件在线预览功能

---
+++ assets/js/oss-config.js
@@ -0,0 +1,9 @@
+// 阿里云OSS配置
+const ossConfig = {
+    region: '您的地域，例如：oss-cn-hangzhou',
+    accessKeyId: '您的AccessKey ID',
+    accessKeySecret: '您的AccessKey Secret',
+    bucket: '您的Bucket名称',
+    // 如果配置了自定义域名，可以在这里设置
+    endpoint: '您的Bucket访问域名'
+}; 
